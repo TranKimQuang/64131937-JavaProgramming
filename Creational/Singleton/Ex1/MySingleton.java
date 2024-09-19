ace988a8b3155e6f95066b4711473ff55f211eac
Trần Kim Quang <162008715+TranKimQuang@users.noreply.github.com>
2024-09-19 17:31:25 +0700

    singleton

---
+++ Creational/Singleton/Ex1/MySingleton.java
@@ -0,0 +1,20 @@
+package Creational.Singleton.Ex1;
+
+public class MySingleton {
+  private static MySingleton instance;
+  int count = 0;
+
+  private MySingleton() {
+  }
+
+  public static MySingleton getInstance() {
+    if (instance == null)
+      instance = new MySingleton();
+      return instance;
+
+  }
+  public void countPrint () {
+    System.out.println(++count);
+  }
+
+}
