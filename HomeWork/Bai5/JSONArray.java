0b7d11b1138f5234dc4eb4d76f2710ec3d516683
Trần Kim Quang <162008715+TranKimQuang@users.noreply.github.com>
2024-10-04 12:01:21 +0700

    Json

---
+++ HomeWork/Bai5/JSONArray.java
@@ -0,0 +1,11 @@
+package HomeWork.Bai5;
+
+public class JSONArray {
+  public void add(JSONObject employeeObject) {
+  }
+
+  public int toJSONString(){
+    return 0;
+  }
+
+}
---
+++ HomeWork/Bai5/JSONObject.java
@@ -0,0 +1,9 @@
+package HomeWork.Bai5;
+
+public class JSONObject {
+  public void put(String firstName, String lokesh) {
+  }
+
+  public void put(String employee, JSONObject employeeDetails) {
+  }
+}
